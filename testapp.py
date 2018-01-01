3ccf04c54adf9969b47e614601e7c65c9f9ebf46
KI1208 <keisuke1208@gmail.com>
2018-01-02 00:21:19 +0900

    Add some function.

--- testapp.py
+++ testapp.py
@@ -1,5 +1,6 @@
 from flask import Flask, current_app, request, flash,redirect,url_for,render_template
 from flask_pymongo import PyMongo
+from dateutil.parser import parse
 
 app = Flask(__name__)
 app.secret_key = 'secret'
@@ -15,16 +16,28 @@ def show_entry():
     users = mongo.db.user.find()
     entries = []
     for row in users:
-        entries.append(row)
+        entries.append({"name": row['name'], "birthday": row['birthday'].strftime("%Y/%m/%d")})
 
     return render_template('toppage.html', entries=entries)
 
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-    mongo.db.user.insert({"name": request.form['name'], "birthday": request.form['birthday']})
+    mongo.db.user.insert({"name": request.form['name'], "birthday": parse(request.form['birthday'])})
     flash('New entry was successfully posted')
     return redirect(url_for('show_entry'))
+
+
+@app.route('/search', methods=['POST'])
+def filter_entry():
+    start = parse(request.form['start'])
+    end = parse(request.form['end'])
+    cur = mongo.db.user.find({'birthday': {'$lt': end, '$gte': start}})
+    results = []
+    for row in cur:
+        results.append({"name": row['name'], "birthday": row['birthday'].strftime("%Y/%m/%d")})
+
+    return render_template('result.html', results=results)
 
 
 if __name__ == '__main__':
